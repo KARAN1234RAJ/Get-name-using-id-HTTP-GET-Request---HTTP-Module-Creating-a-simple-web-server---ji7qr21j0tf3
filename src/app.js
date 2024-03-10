31557c789e0492ffe1afeff5be6c64f82b9ae39d
KARAN1234RAJ <36782443+KARAN1234RAJ@users.noreply.github.com>
2024-03-10 19:29:14 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,34 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 const app = express();
 
 // Importing productNames from names.json file
 const productNames = JSON.parse(
-    fs.readFileSync(`${__dirname}/data/names.json`)
+  fs.readFileSync(`${__dirname}/data/names.json`)
 );
 
 //Middlewares
-app.use(express.json())
+app.use(express.json());
 
 // GET endpoint for sending the products to client by id
 //Endpoint - /api/v1/names/:id
+app.get("/api/v1/names/:id", (req, res) => {
+  // console.log("request receive", req.params);
+  const { id } = req.params; //destructuring assignment
+  console.log("id-->", id);
+  console.log(productNames.map((item) => item.id));
+  if (productNames.map((item) => item.id) === id) {
+    console.log("product id match");
+  } else {
+    console.log("product id doesn't match");
+  }
 
+  const filterData = productNames.filter((item) => item.id == id);
+  res.send({
+    status: 200,
+    message: "Product Name fetched successfully",
+    data: filterData,
+  });
+});
 
 module.exports = app;
